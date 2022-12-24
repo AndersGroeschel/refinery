3cd80022b99dcbe67eaa0216cc119f0fb35f5a3e
Anders Groeschel <andersgroeschel@gmail.com>
2022-12-24 15:18:41 -0500

    added eval rule for primitives

--- src/refinery/eval_rules.v
+++ src/refinery/eval_rules.v
@@ -1,1 +1,10 @@
 Require Import definitions.
+
+
+Reserved Notation "c =R=> p" (at level 40).
+
+Inductive Refinery_Eval_Rule: Refinery_Lang -> R_Lang_Primitive -> Prop :=
+| R_Eval_Prim: forall prim,
+    (R_Primitive prim) =R=> prim
+where "c =R=> p" := (Refinery_Eval_Rule c p). 
+
