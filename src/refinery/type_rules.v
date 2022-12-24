b95a78d03c1f2cb551e810b0594c41ff49c7353e
Anders Groeschel <andersgroeschel@gmail.com>
2022-12-24 15:18:12 -0500

    generalized prim rule

--- src/refinery/type_rules.v
+++ src/refinery/type_rules.v
@@ -5,17 +5,19 @@ Inductive Refinery_BaseType: Type :=
     | R_T_Bool: Refinery_BaseType
 .
 
+Definition typeOf prim :=
+    match prim with 
+    | R_Prim_Bool _ => R_T_Bool
+    end.
+
 Definition Refinery_RefinementType := (Refinery_BaseType * Constraints_Lang)%type.
 
 
 
 Reserved Notation "exp 'hasRefinement' T" (at level 40).
 Inductive Refinery_Type_Rule : Refinery_Lang -> Refinery_RefinementType -> Prop :=
-    | R_T_Rule_True :
-        (R_Primitive (R_Prim_Bool true)) hasRefinement (R_T_Bool, C_Constraint (C_Prop_Self, (C_Op_Equal, R_Prim_Bool true)))
-
-    | R_T_Rule_False :
-        (R_Primitive (R_Prim_Bool false)) hasRefinement (R_T_Bool, C_Constraint (C_Prop_Self,( C_Op_Equal, R_Prim_Bool false)))
+| R_Typing_Prim : forall prim
+    (R_Primitive prim) hasRefinement (typeOf prim, C_Constraint (C_Prop_Self, (C_Op_Equal, prim)))
 
 where "exp 'hasRefinement' T" := (Refinery_Type_Rule exp T).
 
